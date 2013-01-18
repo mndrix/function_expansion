67f00bcce3f3b16ae53b65df94cd8f3e04c69c05
Michael Hendricks <michael@ndrix.org>
2013-01-18 14:46:06 -0700

    Clean up module code
    Remove some dead code that wasn't used at all anymore.  Move some
    manual testing code to a separate file.

--- fun.pl
+++ fun.pl
@@ -38,19 +38,6 @@ $(_,_).
 :- dynamic user:function_expansion/3.
 :- multifile user:function_expansion/3.
 
-function_expansion_loop(T, T) :-  % so `X=fn(1,234)` works
-    var(T),
-    !.
-function_expansion_loop(T0, T) :-
-    user:function_expansion(T0, _, T),
-    !.
-function_expansion_loop(T0, T) :-  % look for expandable terms inside T0
-    nonvar(T0),
-    \+ user:function_expansion(T0, _, _),
-    T0 =.. [Functor|Args0],
-    maplist(function_expansion_loop, Args0, Args),
-    T =.. [Functor|Args].
-
 expand_arglist([], [], []).
 expand_arglist([H0|T0], [H|T], [Guard|Guards]) :-  % leaf
     nonvar(H0),
@@ -100,16 +87,3 @@ user:goal_expansion(T0, T) :-
     T1 =.. [Functor|NewArgs],
     xfy_list(',', Guard, Preconditions),
     T = (Guard, T1).
-
-user:function_expansion(pi, 3.14159, circles_are_round).
-user:function_expansion(hi, X, X = hello).
-user:function_expansion(e, 2.71828, true).
-hmm :-
-    say(hi, pi, bye, more(pi)),
-    (   true -> writeln(e); fail),
-    true.
-
-user:function_expansion(double(X), Y, Y is 2*X).
-ex :-
-    V = 9,
-    format('~p times 2 is ~p~n', [V, double(V)]).
---
+++ manual_test.pl
@@ -0,0 +1,14 @@
+:- use_module(fun).
+
+user:function_expansion(pi, 3.14159, circles_are_round).
+user:function_expansion(hi, X, X = hello).
+user:function_expansion(e, 2.71828, true).
+hmm :-
+    say(hi, pi, bye, more(pi)),
+    (   true -> writeln(e); fail),
+    true.
+
+user:function_expansion(double(X), Y, Y is 2*X).
+ex :-
+    V = 9,
+    format('~p times 2 is ~p~n', [V, double(V)]).
