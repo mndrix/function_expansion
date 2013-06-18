5564e8eac3a3215f7010d495cf0b2726a4b7c82e
Michael Hendricks <michael@ndrix.org>
2013-06-18 14:15:15 -0600

    Bump version

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(function_expansion).
-version('0.1.1').
+version('0.1.2').
 title('Help for writing function-like macros').
 keywords([macro,function]).
 author( 'Michael Hendricks', 'michael@ndrix.org' ).
 packager( 'Michael Hendricks', 'michael@ndrix.org' ).
 maintainer( 'Michael Hendricks', 'michael@ndrix.org' ).
 home('http://packs.ndrix.com/function_expansion/index.html').
-download('http://packs.ndrix.com/function_expansion/function_expansion-0.1.1.tgz').
+download('http://packs.ndrix.com/function_expansion/function_expansion-0.1.2.tgz').
